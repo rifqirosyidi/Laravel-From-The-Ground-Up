e76ef06f28df010246372ba0b6543c4ae93d72f5
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-07-16 22:53:04 +0700

    anchor link refactor

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -5,13 +5,13 @@
   <!-- Links -->
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="about">About</a>
+      <a class="nav-link" href="/about">About</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="contact">Contact</a>
+      <a class="nav-link" href="/contact">Contact</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="customers">Customers</a>
+      <a class="nav-link" href="/customers">Customers</a>
     </li>
   </ul>
 </nav>
